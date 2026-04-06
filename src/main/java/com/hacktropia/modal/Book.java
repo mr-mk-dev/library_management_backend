ecab1317ebc9a9d67249507dcde2c562fb4ac28f
mr-mk-dev <itsmenishan.12@gmail.com>
2026-04-06 15:52:10 +0530

    Corrected id of book

--- src/main/java/com/hacktropia/modal/Book.java
+++ src/main/java/com/hacktropia/modal/Book.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
